4a2bae92ccfbbe38696337a6368ff8b07ff78173
hebron <hebron@tallykhata.com>
2023-05-26 22:04:11 +0600

    more unit test added and some validation added to the controller

--- src/main/java/com/crud/app/rest/Controllers/ApiControllers.java
+++ src/main/java/com/crud/app/rest/Controllers/ApiControllers.java
@@ -3,6 +3,9 @@ package com.crud.app.rest.Controllers;
 import com.crud.app.rest.Models.User;
 import com.crud.app.rest.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 public class ApiControllers {
     @Autowired
     private UserRepo userRepo;
+
     @GetMapping(value = "/")
     public String getPage() {
         return "Hello!! HI";
@@ -20,25 +24,50 @@ public class ApiControllers {
     public List<User> getUsers() {
         return userRepo.findAll();
     }
+    @GetMapping(value = "/user/{id}")
+    public ResponseEntity<?> getUser(@PathVariable long id) {
+        if (userRepo.findById(id).isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(userRepo.findById(id));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No User found for this id");
+        }
+    }
     @PostMapping(value = "/user/create")
-    public String saveUser(@RequestBody User user) {
-        userRepo.save(user);
-        return "User saved to db..";
+    public ResponseEntity<?> saveUser(@RequestBody User user) {
+        if (userRepo.existsByEmail(user.getEmail())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(user.getEmail() + " already exist");
+        } else {
+            if (user.getEmail() == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please provide email address");
+            }
+            userRepo.save(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        }
+
     }
     @PutMapping(value = "/user/update/{id}")
-    public String updateUser(@PathVariable long id, @RequestBody User user) {
-        User toUpdateUser = userRepo.findById(id).get();
-        toUpdateUser.setFirst_name(user.getFirst_name());
-        toUpdateUser.setLast_name(user.getLast_name());
-        toUpdateUser.setProfession(user.getProfession());
-        toUpdateUser.setAge(user.getAge());
-        userRepo.save(toUpdateUser);
-        return "User updated to db..";
+    public ResponseEntity<?> updateUser(@PathVariable long id, @RequestBody User user) {
+        if (userRepo.findById(id).isPresent()) {
+            User toUpdateUser = userRepo.findById(id).get();
+            toUpdateUser.setEmail(user.getEmail());
+            toUpdateUser.setFirst_name(user.getFirst_name());
+            toUpdateUser.setLast_name(user.getLast_name());
+            toUpdateUser.setProfession(user.getProfession());
+            toUpdateUser.setAge(user.getAge());
+            userRepo.save(toUpdateUser);
+            return ResponseEntity.status(HttpStatus.OK).body(userRepo.findById(id));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No User found for this id");
+        }
     }
     @DeleteMapping(value = "user/delete/{id}")
-    public String deleteUser(@PathVariable long id) {
-        User toDeleteUser = userRepo.findById(id).get();
-        userRepo.delete(toDeleteUser);
-        return "User deleted..";
+    public ResponseEntity<?> deleteUser(@PathVariable long id) {
+        if (userRepo.findById(id).isPresent()) {
+            User toDeleteUser = userRepo.findById(id).get();
+            userRepo.delete(toDeleteUser);
+            return ResponseEntity.status(HttpStatus.OK).body("User with ID: " + toDeleteUser.getId() + " deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No User found for this id");
+        }
     }
 }
--- src/test/java/com/crud/app/rest/RestApiApplicationTests.java
+++ src/test/java/com/crud/app/rest/RestApiApplicationTests.java
@@ -3,13 +3,10 @@ package com.crud.app.rest;
 import com.crud.app.rest.Controllers.ApiControllers;
 import com.crud.app.rest.Models.User;
 import com.crud.app.rest.Repo.UserRepo;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.engine.support.discovery.SelectorResolver;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -42,11 +39,12 @@ class RestApiApplicationTests {
 	@MockBean
 	UserRepo userRepo;
 
-	User USER1 = new User(1L, "Park", "Yon", "Developer", 26);
-	User USER2 = new User(2L, "Park", "Bo Gum", "Developer", 28);
-	User USER3 = new User(3L, "Son", "Ye Jin", "Developer", 40);
-	User USER4 = new User(4L, "Kim", "Da Mi", "Developer", 26);
+	User USER1 = new User(1L, "park@yon.com", "Park", "Yon", "Developer", 26);
+	User USER2 = new User(2L, "park@bogum.com", "Park", "Bo Gum", "Developer", 28);
+	User USER3 = new User(3L,"son@yejin.com", "Son", "Ye Jin", "Developer", 40);
+	User USER4 = new User(4L, "kim@dami.com", "Kim", "Da Mi", "Developer", 26);
 
+	private final String GET_SINGLE_USER = "/user/1";
 	private final String GET_ALL_USER = "/users";
 	private final String CREATE_USER = "/user/create";
 	private final String UPDATE_USER = "/user/update/1";
@@ -69,16 +67,51 @@ class RestApiApplicationTests {
 
 		int status = mvcResult.getResponse().getStatus();
 		String body = mvcResult.getResponse().getContentAsString();
-//		System.out.println("Statusssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss "+ objectMapper.readValue(body, new TypeReference<List<User>>(){}).get(1).getFirst_name());
+//		System.out.println("Statussssss "+ objectMapper.readValue(body, new TypeReference<List<User>>(){}).get(1).getFirst_name());
 		assertEquals(200, status);
 	}
 
-	@Test
+	@Test()
 	@Order(2)
-	void createUser() throws Exception {
-		User userToCreate = new User(5L, "Bo", "Young", "Actress", 35);
+	void getSingleUser() throws Exception {
+		Mockito.when(userRepo.findById(USER1.getId())).thenReturn(Optional.of(USER1));
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.get(GET_SINGLE_USER)
+				.contentType(MediaType.APPLICATION_JSON);
+		MvcResult mvcResult = mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.first_name", Matchers.is("Park")))
+				.andReturn();
 
-		Mockito.when(userRepo.save(userToCreate)).thenReturn(userToCreate);
+		int status = mvcResult.getResponse().getStatus();
+		String body = mvcResult.getResponse().getContentAsString();
+		assertEquals(200, status);
+	}
+
+	@Test()
+	@Order(3)
+	void getUserNotFound() throws Exception {
+//		Mockito.when(userRepo.findById(USER3.getId())).thenReturn(Optional.of(USER3));
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.get(GET_SINGLE_USER)
+				.contentType(MediaType.APPLICATION_JSON);
+		MvcResult mvcResult = mockMvc.perform(request)
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$", Matchers.is("No User found for this id")))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		String body = mvcResult.getResponse().getContentAsString();
+		assertEquals(400, status);
+		assertEquals(body, "No User found for this id");
+	}
+
+	@Test
+	@Order(4)
+	void createUser() throws Exception {
+		User userToCreate = new User(5L, "bo@young.com", "Bo", "Young", "Actress", 35);
+
+//		Mockito.when(userRepo.save(userToCreate)).thenReturn(userToCreate);
 		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
 				.post(CREATE_USER)
 				.contentType(MediaType.APPLICATION_JSON)
@@ -86,18 +119,43 @@ class RestApiApplicationTests {
 				.content(objectMapper.writeValueAsString(userToCreate));
 
 		MvcResult mvcResult = mockMvc.perform(request)
-				.andExpect(status().isOk())
+				.andExpect(status().isCreated())
 				.andExpect(jsonPath("$", notNullValue()))
-//				.andExpect(jsonPath("$.first_name", Matchers.is("Bo")))
+				.andExpect(jsonPath("$.first_name", Matchers.is("Bo")))
 				.andReturn();
+
 		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
+		assertEquals(201, status);
 	}
 
 	@Test
-	@Order(3)
+	@Order(5)
+	void createUserWithoutEmail() throws Exception {
+		User userToCreate = new User(5L, "Bo", "Young", "Actress", 35);
+
+//		Mockito.when(userRepo.save(userToCreate)).thenReturn(userToCreate);
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.post(CREATE_USER)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(userToCreate));
+
+		MvcResult mvcResult = mockMvc.perform(request)
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$", notNullValue()))
+				.andExpect(jsonPath("$", Matchers.is("Please provide email address")))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		String body = mvcResult.getResponse().getContentAsString();
+		assertEquals(400, status);
+		assertEquals(body, "Please provide email address");
+	}
+
+	@Test
+	@Order(6)
 	void updateUser() throws Exception {
-		User userToUpdate = new User(1L, "Park", "Bo Young", "Actress", 35);
+		User userToUpdate = new User(1L,"park@boyoung.com", "Park", "Bo Young", "Actress", 35);
 
 		Mockito.when(userRepo.findById(USER1.getId())).thenReturn(Optional.of(USER1));
 		Mockito.when(userRepo.save(userToUpdate)).thenReturn(userToUpdate);
@@ -112,12 +170,38 @@ class RestApiApplicationTests {
 				.andExpect(jsonPath("$", notNullValue()))
 //				.andExpect(jsonPath("$.first_name", Matchers.is("Bo")))
 				.andReturn();
+
 		int status = mvcResult.getResponse().getStatus();
 		assertEquals(200, status);
 	}
 
 	@Test
-	@Order(4)
+	@Order(7)
+	void updateUserNotExist() throws Exception {
+		User userToUpdate = new User(5L,"park@boyoung.com", "Park", "Bo Young", "Actress", 35);
+
+//		Mockito.when(userRepo.findById(USER3.getId())).thenReturn(Optional.of(USER3));
+//		Mockito.when(userRepo.save(userToUpdate)).thenReturn(userToUpdate);
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.put(UPDATE_USER)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(userToUpdate));
+
+		MvcResult mvcResult = mockMvc.perform(request)
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$", notNullValue()))
+				.andExpect(jsonPath("$", Matchers.is("No User found for this id")))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		String body = mvcResult.getResponse().getContentAsString();
+		assertEquals(400, status);
+		assertEquals(body, "No User found for this id");
+	}
+
+	@Test
+	@Order(8)
 	void deleteUser() throws Exception {
 		Mockito.when(userRepo.findById(USER2.getId())).thenReturn(Optional.of(USER2));
 
@@ -128,8 +212,25 @@ class RestApiApplicationTests {
 		MvcResult mvcResult = mockMvc.perform(request)
 				.andExpect(status().isOk())
 				.andReturn();
+
 		int status = mvcResult.getResponse().getStatus();
 		assertEquals(200, status);
+	}
+	@Test
+	@Order(9)
+	void deleteUserNotExist() throws Exception {
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.delete(DELETE_USER)
+				.contentType(MediaType.APPLICATION_JSON);
+
+		MvcResult mvcResult = mockMvc.perform(request)
+				.andExpect(status().isBadRequest())
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		String body = mvcResult.getResponse().getContentAsString();
+		assertEquals(400, status);
+		assertEquals(body, "No User found for this id");
 	}
 
 }
